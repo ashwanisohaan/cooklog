73485828f8ed8c2ec105eb8f01fa9f99cbe754e1
Sukhwantsingh <singh.sukhwant68@gmail.com>
2025-04-08 10:04:01 +0530

    simple routing

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key, required this.onP});
+
+  VoidCallback onP;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Demo App',
-          style: TextStyle(
-            color: Colors.amber,
-            fontSize: 24,
-          ),
+          style: TextStyle(color: Colors.amber, fontSize: 24),
         ),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.file_copy),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {},
-          )
+          IconButton(icon: Icon(Icons.file_copy), onPressed: () {}),
+          IconButton(icon: Icon(Icons.notifications), onPressed: () {})
         ],
         leading: IconButton(
-          icon: const Icon(Icons.photo),
-          tooltip: 'Menu Icon',
-          onPressed: () {},
-        ),
+            icon: const Icon(Icons.photo),
+            tooltip: 'Menu Icon',
+            onPressed: () {}),
         backgroundColor: Colors.red,
       ),
       body: Container(
@@ -82,11 +76,12 @@ class LoginScreen extends StatelessWidget {
           ),
           ElevatedButton(
             child: Text('Log In'),
-            onPressed: () {},
+            onPressed: () {
+              onP();
+            },
           )
         ]),
       ),
     );
-    ;
   }
 }
